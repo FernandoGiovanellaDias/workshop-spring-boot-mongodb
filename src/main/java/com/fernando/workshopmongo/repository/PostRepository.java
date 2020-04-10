99486285f240192440a9d7be01511958bbb70074
FernandoGiovanellaDias <fagdias2002@hotmail.com>
2020-04-10 10:23:43 -0300

    Consulta com varios critérios

--- src/main/java/com/fernando/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/fernando/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.fernando.workshopmongo.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -10,9 +11,13 @@ import com.fernando.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+
 	@Query("{ 'title' : { $regex: ?0, $options: 'i'} }")
 	List<Post> findByTitle(String text);
-	
+
 	List<Post> findByTitleContainingIgnoreCase(String text);
+
+	@Query("{ $and: [ { date: {$gte: ?1} }, { date: {$lte: ?2} } , { $or: [ {'title' : { $regex: ?0, $options: 'i'} }, {'body' : { $regex: ?0, $options: 'i'}}, {'comments.text' : { $regex: ?0, $options: 'i'}} ] } ] }")
+	List<Post> fullSerch(String text, Date minDate, Date maxDate);
 
 }
